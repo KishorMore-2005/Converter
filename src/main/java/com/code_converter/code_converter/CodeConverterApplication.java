bdcf273b25b68f40090d8cdd75c4f2b8b56a01f4
KishorMore <kishormoresuresh123@gmail.com>
2025-08-25 19:34:16 +0530

    Adding project structure

---
+++ src/main/java/com/code_converter/code_converter/CodeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.code_converter.code_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code_converter/code_converter/CodeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code_converter.code_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
